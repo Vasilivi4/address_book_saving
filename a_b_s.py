eab6e25807a00394439e84c2cb1602601f6c88d7
Vasilivi4 <137120278+Vasilivi4@users.noreply.github.com>
2023-10-09 21:53:28 +0300

    Update a_b_s.py

--- a_b_s.py
+++ a_b_s.py
@@ -6,18 +6,46 @@ class Field:
         self.value = value
 
 
-class Phone(Field):
-    pass
-
-
 class Name(Field):
-    pass
+    def __init__(self, value):
+        super().__init__(value)
+
+    def __str__(self):
+        return self.value
+
+
+class Phone(Field):
+    def __init__(self, value):
+        super().__init__(value)
+        self.validate_phone()
+
+    def validate_phone(self):
+        if not self.value.isdigit() or len(self.value) != 10:
+            raise ValueError("Номер телефону повинен містити 10 цифр")
 
 
 class Record:
-    def __init__(self, name, phone):
-        self.name = name
-        self.phone = phone
+    def __init__(self, name):
+        self.name = name.value
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        if old_phone not in [p.value for p in self.phones]:
+            raise ValueError("Номер телефону для редагування не існує")
+
+        self.remove_phone(old_phone)
+        self.add_phone(new_phone)
+
+    def find_phone(self, phone):
+        for p in self.phones:
+            if p.value == phone:
+                return p
 
 
 class AddressBook:
@@ -42,9 +70,11 @@ class AddressBook:
         results = []
         for contact in self.contacts:
             if (
-                query.lower() in contact.name.value.lower()
-                or query in contact.phone.value
-            ):
+                isinstance(
+                    contact.name, str
+                )  # Проверяем, является ли значение имени строкой
+                and query.lower() in contact.name.lower()
+            ) or any(query in phone.value for phone in contact.phones):
                 results.append(contact)
         return results
 
@@ -57,25 +87,29 @@ def main():
         print("\nMenu:")
         print("1. Add contact")
         print("2. Search contacts")
-        print("3. Exit")
+        print("3. Save and Exit")
         choice = input("Select option: ")
 
         if choice == "1":
             name_value = input("Enter a name for the contact: ")
             phone_value = input("Enter phone number for contact: ")
             name = Name(name_value)
-            phone = Phone(phone_value)
-            new_contact = Record(name, phone)
+            new_contact = Record(name)
+            new_contact.add_phone(phone_value)  # Добавляем номер телефона отдельно
             address_book.add_contact(new_contact)
             print("Contact added!")
 
-        elif choice == "2":
+        if choice == "2":
             query = input("Enter a search term to search: ")
-            results = address_book.search_contacts(query)
+            results = address_book.search_contacts(
+                query.lower()
+            )  # Преобразуем запрос к нижнему регистру
             if results:
                 print("Search results:")
                 for contact in results:
-                    print(f"Name: {contact.name.value}, Phone: {contact.phone.value}")
+                    print(
+                        f"Name: {contact.name}, Phone: {contact.phones[0].value}"
+                    )  # Используем первый номер телефона
             else:
                 print("Contact not found.")
 
